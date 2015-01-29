04e6f67958587f5238d82b2cdc9b6c7af99a278c
NathanaelChow <nhchowder@gmail.com>
2015-01-29 09:13:26 -0800

    Finished milestones 1-9 and started 10

---
+++ controller/create-db.php
@@ -0,0 +1,14 @@
+<?php
+    repuire_once("../model/database.php");
+    
+    $connection = new mysqli($host, $username, $password);
+
+    if($connection->connect_error){
+        die("error: " . $connection->connect_error);
+    }
+    else{
+        echo "success: " . $connection->host_info;
+    }
+
+    $connection->close();
+            
