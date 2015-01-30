05087ea8d8c6ff8118fb59686d6ba3d1205ac273
NathanaelChow <nhchowder@gmail.com>
2015-01-30 09:15:28 -0800

    Completed milestone videos 10, 11, 12

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,14 +1,21 @@
 <?php
-    repuire_once("../model/database.php");
+    require_once(__DIR__ . "/../model/database.php");
     
     $connection = new mysqli($host, $username, $password);
 
     if($connection->connect_error){
-        die("error: " . $connection->connect_error);
-    }
-    else{
-        echo "success: " . $connection->host_info;
+        die("Error: " . $connection->connect_error);
     }
 
+    $exists = $connection->select_db($database); 
+    
+    if(!$exists){
+        $query = $connection->query("CREATE DATABASE $database");
+   
+        if($query){
+            echo "Successfully created database: " . $database;
+        }
+    }
+    
     $connection->close(); 
             
